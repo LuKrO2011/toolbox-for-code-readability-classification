0f2c81c3310e1d390cc95d2acd9f7fbebb67e2f5
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2023-12-11 12:35:46 +0100

    Refactoring method into multiple ones

--- src/readability_preprocessing/extractors/sampled_extractor.py
+++ src/readability_preprocessing/extractors/sampled_extractor.py
@@ -19,53 +19,85 @@ def extract_sampled(input_dirs: list[Path], output_dir: Path,
     # Load the sampling files
     strata_names = [file for file in sampling_dir.iterdir() if file.is_file()]
 
-    # Create a subdirectory for each filename in the sampling files
+    # Create the subdirectories for each sampling file and input directory
     for stratum_name in strata_names:
-        sampling_file_name = stratum_name.stem
-        sub_dir_name = output_dir / sampling_file_name
-        sub_dir_name.mkdir(parents=True, exist_ok=True)
+        _create_subdirectories(output_dir, stratum_name, input_dirs)
 
-        # Create a subsubdirectory in each subdirectory for each input_dir path
-        for input_dir in input_dirs:
-            input_dir_name = input_dir.stem
-            sub_sub_dir_name = sub_dir_name / input_dir_name
-            sub_sub_dir_name.mkdir(parents=True, exist_ok=True)
-
-    # Load the content of each sampling file
+    # Load the contents of the sampling files
     strata_contents = [file.read_text().splitlines() for file in strata_names]
     strata_contents = _to_relative_paths(strata_contents)
     stratas_with_names = list(zip(strata_names, strata_contents))
 
     # Copy the files to the output directory
+    _copy_files_to_output(input_dirs, output_dir, stratas_with_names)
+
+
+def _create_subdirectories(output_dir: Path, sampling_file: Path,
+                           input_dirs: list[Path]) -> None:
+    """
+    Creates the subdirectories for the given sampling file and input directories.
+    :param output_dir: The output directory.
+    :param sampling_file: The sampling file.
+    :param input_dirs: The input directories.
+    :return: None.
+    """
+    sampling_file_name = sampling_file.stem
+    sub_dir_name = output_dir / sampling_file_name
+    sub_dir_name.mkdir(parents=True, exist_ok=True)
+
+    for input_dir in input_dirs:
+        input_dir_name = input_dir.stem
+        sub_sub_dir_name = sub_dir_name / input_dir_name
+        sub_sub_dir_name.mkdir(parents=True, exist_ok=True)
+
+
+def _copy_files_to_output(input_dirs: list[Path], output_dir: Path,
+                          stratas_with_names: list) -> None:
+    """
+    Copies the files to the output directories.
+    :param input_dirs: The directories to extract the sampled files from.
+    :param output_dir: The directory to extract the sampled files to.
+    :param stratas_with_names: The stratas with names.
+    :return: None.
+    """
     for input_dir in input_dirs:
         for root, dirs, files in os.walk(input_dir):
             for file_name in files:
-
-                # Get the absolute path of the file to the system root
                 absolute_file_path = os.path.join(root, file_name)
                 absolute_file_path = str(Path(absolute_file_path).absolute())
 
-                # Check if the file is in any of the sampling files
-                for name, stratum in stratas_with_names:
-                    if _check_path_in(absolute_file_path, stratum):
-                        # Copy the file to the output directory
-                        input_file_path = absolute_file_path
-
-                        new_file_name = _calculate_new_file_name(
-                            file_path=input_file_path,
-                            input_dir=input_dir
-                        )
-
-                        output_file_path = os.path.join(
-                            output_dir,
-                            name.stem,
-                            input_dir.stem,
-                            new_file_name
-                        )
-                        shutil.copy(input_file_path, output_file_path)
+                _copy_matching_files(absolute_file_path, stratas_with_names, output_dir,
+                                     input_dir)
 
 
-def _calculate_new_file_name(file_path: str, input_dir: Path) -> str:
+def _copy_matching_files(input_file_path: str, stratas_with_names: list,
+                         output_dir: Path,
+                         input_dir: Path) -> None:
+    """
+    Copies the files to the output directories.
+    :param input_file_path: The input file path.
+    :param stratas_with_names: The stratas with names.
+    :param output_dir: The directory to extract the sampled files to.
+    :param input_dir: The input directory.
+    :return: None.
+    """
+    for name, stratum in stratas_with_names:
+        if _check_path_in(input_file_path, stratum):
+            new_file_name = _get_new_file_name(
+                file_path=input_file_path,
+                input_dir=input_dir
+            )
+
+            output_file_path = os.path.join(
+                output_dir,
+                name.stem,
+                input_dir.stem,
+                new_file_name
+            )
+            shutil.copy(input_file_path, output_file_path)
+
+
+def _get_new_file_name(file_path: str, input_dir: Path) -> str:
     """
     Calculates the new file name for the file at the given file path.
     :param file_path: The file path of the file.
