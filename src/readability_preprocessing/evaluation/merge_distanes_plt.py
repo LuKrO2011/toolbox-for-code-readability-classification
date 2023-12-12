f3e82725ca83be60ca8fd9310487d9a470ce592f
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2023-12-12 15:24:43 +0100

    Improving plot heading naming

--- src/readability_preprocessing/evaluation/merge_distanes_plt.py
+++ src/readability_preprocessing/evaluation/merge_distanes_plt.py
@@ -12,7 +12,7 @@ REVERSE_X_AXIS = True
 PLOT_DIFF_TO_PREV = True
 
 if PLOT_DIFF_TO_PREV:
-    title = 'Merge Distances and Diff to Previous vs Number of Stratas'
+    title = 'Merge Distances and Local Derivation vs Number of Stratas'
 else:
     title = 'Merge Distances vs Number of Stratas'
 
@@ -39,25 +39,47 @@ ax1.grid(True)
 if PLOT_DIFF_TO_PREV:
     ax2 = ax1.twinx()
     ax2.plot(num_stratas, diff_to_prev, marker='s', linestyle='-', color='r',
-             label='Diff to Previous')
-    ax2.set_ylabel('Diff to Previous', color='r')
+             label='Local Derivation')
+    ax2.set_ylabel('Local Derivation', color='r')
     ax2.tick_params('y', colors='r')
 
-# Reverse x-axis if specified
+# Invert the x-axis if specified
 if REVERSE_X_AXIS:
+    fig, ax1 = plt.subplots(figsize=(8, 6))
+
+    # Plot merge distances on the first y-axis
+    ax1.plot(num_stratas, merge_distances, marker='o', linestyle='-', color='b',
+             label='Merge Distances')
+    ax1.set_xlabel('Number of Stratas')
+    ax1.set_ylabel('Merge Distances', color='b')
+    ax1.tick_params('y', colors='b')
+    ax1.grid(True)
+
+    # Create a second y-axis for diff_to_prev
+    if PLOT_DIFF_TO_PREV:
+        ax2 = ax1.twinx()
+        ax2.plot(num_stratas, diff_to_prev, marker='s', linestyle='-', color='r',
+                 label='Local Derivation')
+        ax2.set_ylabel('Local Derivation', color='r')
+        ax2.tick_params('y', colors='r')
+
+    # Invert the x-axis for ax1
     ax1.invert_xaxis()
-    plt.xticks(num_stratas, [str(i) for i in reversed(num_stratas)])  # Reversed x-axis ticks and labels
 
-# Combine legends
-lines_1, labels_1 = ax1.get_legend_handles_labels()
+    # Combine legends
+    lines_1, labels_1 = ax1.get_legend_handles_labels()
 
-if PLOT_DIFF_TO_PREV:
-    lines_2, labels_2 = ax2.get_legend_handles_labels()
-    ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc='upper right')
+    if PLOT_DIFF_TO_PREV:
+        lines_2, labels_2 = ax2.get_legend_handles_labels()
+        ax1.legend(lines_1 + lines_2, labels_1 + labels_2, loc='upper right')
+    else:
+        ax1.legend(lines_1, labels_1, loc='upper right')
+
+    plt.xticks(list(reversed(num_stratas)), [str(i) for i in reversed(num_stratas)])
+
+    plt.title(title)
+    plt.tight_layout()
+    plt.show()
+
 else:
-    ax1.legend(lines_1, labels_1, loc='upper right')
-
-# Show the plot
-plt.title(title)
-plt.tight_layout()
-plt.show()
+    print('Not implemented yet')
