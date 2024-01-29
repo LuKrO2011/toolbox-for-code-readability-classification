b312926312f09bb66134c5720401e0442a30d74e
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-01-29 14:17:07 +0100

    5 plots working

--- src/readability_preprocessing/evaluation/sampling_diff_plt.py
+++ src/readability_preprocessing/evaluation/sampling_diff_plt.py
@@ -4,43 +4,49 @@ from readability_preprocessing.evaluation.utils import NONE_DIFF_RESULTS_DIR, \
     load_json_file
 
 
-def generate_bar_chart(data: list) -> plt:
+def generate_bar_chart(data: list,
+                       headline: str = 'Relative Frequency of "Not Different" Cases') \
+    -> None:
     """
     Generates a bar chart for the relative frequency of 'not_different' cases overall,
     for each stratum, and for each stratum and rdh.
     :param data: The data containing the relative frequency of 'not_different' cases
+    :param headline: The headline of the bar chart
     """
     # Extract relevant data for plotting
     strata_labels = [entry["stratum"] for entry in data]
     not_different = [entry["not_different_rel"] for entry in data]
 
     # Create a bar chart for 'different' cases
-    fig, ax = plt.subplots(figsize=(10, 8))  # Increase the height of the plot
+    # fig, ax = plt.subplots(figsize=(10, 8))  # Increase the height of the plot
     plt.bar(strata_labels, not_different, color='lightcoral', edgecolor='black')
 
     # Adding labels, title, and legend
-    plt.xlabel('Stratum and RDH')
+    # plt.xlabel('Stratum (and RDH)')
     plt.ylabel('Relative Frequency')
-    plt.title("Relative Frequency of 'Not Different' Cases Compared To 'None'")
+    plt.title(headline)
 
     # Display the percentage values on top of the bars with smaller font size
     for i, v in enumerate(not_different):
         plt.text(i, v, f'{v:.1%}', ha='center', va='bottom', color='black',
-                 fontweight='bold', fontsize=10)
+                 fontweight='bold', fontsize=8)
+
+    # Fix the y-scale to be between 0 and 1
+    plt.ylim(0, 1)
 
     plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
-    plt.subplots_adjust(bottom=0.2)  # Increase space at the bottom
+    # plt.subplots_adjust(bottom=0.2)  # Increase space at the bottom
 
     # Show the plot
     plt.tight_layout()
-    return plt
+    plt.show()
 
 
 diff_dir = NONE_DIFF_RESULTS_DIR / "statistics.json"
 diff_data = load_json_file(diff_dir)
-plt = generate_bar_chart(diff_data)
-plt.show()
-
+generate_bar_chart(diff_data,
+                   headline='Relative Frequency of "Not Different" Cases Overall')
 for stratum in diff_data[1:]:
-    plt = generate_bar_chart(stratum["sub_statistics"])
-    plt.show()
+    generate_bar_chart(stratum["sub_statistics"],
+                       headline=f'Relative Frequency of "Not Different" Cases for '
+                                f'{stratum["stratum"]}')
