f595d1db60267d7d1b43cd5961637ef3eb572699
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-01-29 13:42:33 +0100

    Only printing differnt bars

--- src/readability_preprocessing/evaluation/sampling_diff_plt.py
+++ src/readability_preprocessing/evaluation/sampling_diff_plt.py
@@ -4,39 +4,29 @@ from readability_preprocessing.evaluation.utils import NONE_DIFF_RESULTS_DIR, \
     load_json_file
 
 
-def generate_stacked_bar_chart(data: list) -> None:
+def generate_bar_chart(data: list) -> None:
     """
-    Generates a stacked bar chart.
-    :param data: The data to plot
-    :return: The plot
+    Generates a bar chart for the relative frequency of 'not_different' cases overall,
+    for each stratum, and for each stratum and rdh.
+    :param data: The data containing the relative frequency of 'not_different' cases
     """
     # Extract relevant data for plotting
     strata_labels = [entry["stratum"] for entry in data]
-    not_different_rel_values = [entry["not_different_rel"] for entry in data]
-    different_rel_values = [entry["different_rel"] for entry in data]
+    not_different = [entry["not_different_rel"] for entry in data]
 
-    # Create a stacked bar chart
+    # Create a bar chart for 'different' cases
     fig, ax = plt.subplots(figsize=(10, 6))
-    bar_width = 0.8
-
-    # Plotting the bars
-    bars1 = plt.bar(strata_labels, not_different_rel_values, color='lightblue',
-                    edgecolor='black', width=bar_width, label='Not Different')
-    bars2 = plt.bar(strata_labels, different_rel_values,
-                    bottom=not_different_rel_values, color='lightcoral',
-                    edgecolor='black', width=bar_width, label='Different')
+    plt.bar(strata_labels, not_different, color='lightcoral', edgecolor='black')
 
     # Adding labels, title, and legend
     plt.xlabel('Stratum')
     plt.ylabel('Relative Frequency')
-    plt.title('Relative Frequency of Different and Not Different Cases in Each Stratum')
-    plt.legend()
+    plt.title('Relative Frequency of Not Different Cases')
 
     # Display the percentage values on top of the bars
-    for bar in bars1 + bars2:
-        yval = bar.get_height()
-        plt.text(bar.get_x() + bar.get_width() / 2, yval, f'{yval:.2%}', ha='center',
-                 va='bottom', color='black', fontweight='bold')
+    for i, v in enumerate(not_different):
+        plt.text(i, v, f'{v:.2%}', ha='center', va='bottom', color='black',
+                 fontweight='bold')
 
     # Show the plot
     plt.tight_layout()
@@ -46,4 +36,4 @@ def generate_stacked_bar_chart(data: list) -> None:
 
 diff_dir = NONE_DIFF_RESULTS_DIR / "statistics.json"
 diff_data = load_json_file(diff_dir)
-generate_stacked_bar_chart(diff_data)
+generate_bar_chart(diff_data)
