1140cebb330f61856e04797754e5036bd23bc1d7
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-02-05 10:30:07 +0100

    Plotting works as well

--- src/readability_preprocessing/evaluation/prolific_time_plt.py
+++ src/readability_preprocessing/evaluation/prolific_time_plt.py
@@ -14,15 +14,6 @@ df = df[df['Status'] != 'TIMED-OUT']
 # Get the time taken to complete the survey
 time_taken = pd.to_timedelta(df['Time taken'], unit="s")
 
-# Plot the boxplot
-plt.figure(figsize=(10, 6))
-boxplot = plt.boxplot(time_taken, vert=False, patch_artist=True)
-
-# Customize the boxplot
-plt.title('Time Taken to Complete Survey')
-plt.xlabel('Time (mm:ss)')
-plt.yticks([])  # Hide y-axis ticks
-
 # Calculate and display mean, std, and median
 mean_time = time_taken.mean().total_seconds()
 std_time = time_taken.std().total_seconds()
@@ -33,5 +24,16 @@ print(f"Mean time: {mean_time // 60:.0f}:{mean_time % 60:.0f}")
 print(f"Std time: {std_time // 60:.0f}:{std_time % 60:.0f}")
 print(f"Median time: {median_time // 60:.0f}:{median_time % 60:.0f}")
 
-# Show the plot
+# Plotting
+time_in_seconds = time_taken.dt.total_seconds()
+plt.figure(figsize=(6, 8))
+plt.boxplot(time_in_seconds)
+plt.xticks([1], ['Overall'])
+plt.ylabel('Time (minutes)')
+plt.title('Time required to complete the survey')
+
+# Update y-axis labels
+plt.yticks(plt.yticks()[0],
+           ['{}:{:02d}'.format(int(seconds // 60), int(seconds % 60)) for seconds in
+            plt.yticks()[0]])
 plt.show()
