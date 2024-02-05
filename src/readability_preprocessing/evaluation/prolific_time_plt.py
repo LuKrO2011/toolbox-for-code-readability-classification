a76909821e79a51bdadf9c32897b22db8bc7b532
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-02-05 13:27:56 +0100

    Adding additional filters for data

--- src/readability_preprocessing/evaluation/prolific_time_plt.py
+++ src/readability_preprocessing/evaluation/prolific_time_plt.py
@@ -5,11 +5,15 @@ from readability_preprocessing.evaluation.utils import load_csv_file, \
     DEMOGRAPHIC_DATA_DIR
 
 # Load the CSV file into a DataFrame
-file_path = DEMOGRAPHIC_DATA_DIR / "p1s0.csv"
+file_path = DEMOGRAPHIC_DATA_DIR / "p1s1.csv"
 df = load_csv_file(file_path)
 
-# Remove all samples which have "TIMED-OUT" as status
-df = df[df['Status'] != 'TIMED-OUT']
+# Remove all non-completed surveys
+not_completed_codes = ['TIMED-OUT', 'RETURNED']
+df = df[~df['Status'].isin(not_completed_codes)]
+
+# Remove all surveys with a "NOCODE" Completion code
+df = df[df['Completion code'] != 'NOCODE']
 
 # Get the time taken to complete the survey
 time_taken = pd.to_timedelta(df['Time taken'], unit="s")
@@ -24,7 +28,7 @@ print(f"Mean time: {mean_time // 60:.0f}:{mean_time % 60:.0f}")
 print(f"Std time: {std_time // 60:.0f}:{std_time % 60:.0f}")
 print(f"Median time: {median_time // 60:.0f}:{median_time % 60:.0f}")
 
-# Plotting
+# Plotting (orange line = median)
 time_in_seconds = time_taken.dt.total_seconds()
 plt.figure(figsize=(6, 8))
 plt.boxplot(time_in_seconds)
