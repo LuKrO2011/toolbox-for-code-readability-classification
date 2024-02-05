93a59650799f3c30630239339c4f18b34fdf348b
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-02-05 09:34:48 +0100

    Printing the mean, std and median works

--- src/readability_preprocessing/evaluation/prolific_time_plt.py
+++ src/readability_preprocessing/evaluation/prolific_time_plt.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 from readability_preprocessing.evaluation.utils import load_csv_file, \
     DEMOGRAPHIC_DATA_DIR
 
@@ -15,23 +14,24 @@ df = df[df['Status'] != 'TIMED-OUT']
 # Get the time taken to complete the survey
 time_taken = pd.to_timedelta(df['Time taken'], unit="s")
 
-# # Plot the boxplot
-# plt.figure(figsize=(10, 6))
-# boxplot = plt.boxplot(time_taken, vert=False, patch_artist=True)
-#
-# # Customize the boxplot
-# plt.title('Time Taken to Complete Survey')
-# plt.xlabel('Time (mm:ss)')
-# plt.yticks([])  # Hide y-axis ticks
+# Plot the boxplot
+plt.figure(figsize=(10, 6))
+boxplot = plt.boxplot(time_taken, vert=False, patch_artist=True)
+
+# Customize the boxplot
+plt.title('Time Taken to Complete Survey')
+plt.xlabel('Time (mm:ss)')
+plt.yticks([])  # Hide y-axis ticks
 
 # Calculate and display mean, std, and median
-mean_time = time_taken.mean()
-std_time = time_taken.std()
-median_time = time_taken.median()
+mean_time = time_taken.mean().total_seconds()
+std_time = time_taken.std().total_seconds()
+median_time = time_taken.median().total_seconds()
 
-print(f"Mean Time: {str(mean_time).split()[-1]}")
-print(f"Standard Deviation: {str(std_time).split()[-1]}")
-print(f"Median Time: {str(median_time).split()[-1]}")
+# Print as "mm:ss"
+print(f"Mean time: {mean_time // 60:.0f}:{mean_time % 60:.0f}")
+print(f"Std time: {std_time // 60:.0f}:{std_time % 60:.0f}")
+print(f"Median time: {median_time // 60:.0f}:{median_time % 60:.0f}")
 
 # Show the plot
 plt.show()
