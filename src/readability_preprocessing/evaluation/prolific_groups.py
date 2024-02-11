1425518aa16eb04e22f4181835547d1dc02b330b
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-02-11 15:54:15 +0100

    Fixing prolific group loader

--- src/readability_preprocessing/evaluation/prolific_groups.py
+++ src/readability_preprocessing/evaluation/prolific_groups.py
@@ -1,5 +1,6 @@
 import csv
 from datetime import datetime
+from pathlib import Path
 
 from readability_preprocessing.evaluation.utils import DEMOGRAPHIC_DATA_DIR
 
@@ -28,12 +29,15 @@ class Submission:
         self.archived_at = datetime.strptime(data['Archived at'],
                                              '%Y-%m-%dT%H:%M:%S.%fZ') if data[
             'Archived at'] else None
-        self.time_taken = float(data['Time taken'])
-        self.completion_code = data['Completion code']
-        self.total_approvals = int(data['Total approvals'])
+        self.time_taken = float(data['Time taken']) if data['Time taken'] else None
+        self.completion_code = data['Completion code'] if data[
+            'Completion code'] else None
+        self.total_approvals = int(data['Total approvals']) if data[
+            'Total approvals'] else None
         self.programming_languages = [lang.strip() for lang in
                                       data['Programming languages'].split(',')]
-        self.age = int(data['Age'])
+        self.age = int(data['Age']) if data['Age'] and data[
+            'Age'] != 'CONSENT_REVOKED' else None
         self.sex = data['Sex']
         self.ethnicity = data['Ethnicity simplified']
         self.country_of_birth = data['Country of birth']
@@ -56,7 +60,7 @@ class CSVProcessor:
     A class to process the CSV file of a survey.
     """
 
-    def __init__(self, file_path: str) -> None:
+    def __init__(self, file_path: Path) -> None:
         """
         Initialize the CSV processor with the file path.
         :param file_path: The path to the CSV file
@@ -76,17 +80,38 @@ class CSVProcessor:
                 self.submissions.append(submission)
 
 
-def load_submissions(input_path: str) -> list[Submission]:
+def load_submissions(input_path: Path) -> list[Submission]:
     """
     Load all submissions from a CSV file and return a list of submission objects.
     :param input_path: The path to the CSV file
     :return: A list of submission objects
     """
-    csv_processor = CSVProcessor(input_path)
-    csv_processor.process_csv()
-    return csv_processor.submissions
+    if input_path.is_file():  # Load a single CSV file
+        csv_processor = CSVProcessor(input_path)
+        csv_processor.process_csv()
+        return csv_processor.submissions
+    else:  # Load all CSV files
+        file_paths = list(input_path.glob('*.csv'))
+        submissions = []
+        for file_path in file_paths:
+            csv_processor = CSVProcessor(file_path)
+            csv_processor.process_csv()
+            submissions.extend(csv_processor.submissions)
+        return submissions
 
 
-submissions = load_submissions(DEMOGRAPHIC_DATA_DIR / "p1s0.csv")
-for submission in submissions:
-    print(submission)
+def filter_submissions_by_status(submissions: list[Submission], to_remove: list[str]) -> \
+    list[Submission]:
+    """
+    Filter the submissions by the status of the submission.
+    :param submissions: The list of submissions
+    :param to_remove: The list of statuses to remove
+    :return: The filtered list of submissions
+    """
+    return [submission for submission in submissions if
+            submission.status not in to_remove]
+
+
+submissions = load_submissions(DEMOGRAPHIC_DATA_DIR)
+submissions = filter_submissions_by_status(submissions, ['TIMED-OUT', 'RETURNED'])
+print(len(submissions))
