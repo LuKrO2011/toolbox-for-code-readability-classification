e52113df684ee37b3b05baa2d9c02391e02005f6
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-02-11 12:30:08 +0100

    Parsing json to objects and combining multiple demographic files

--- src/readability_preprocessing/evaluation/prolific_demographic_data.py
+++ src/readability_preprocessing/evaluation/prolific_demographic_data.py
@@ -21,6 +21,15 @@ class Answer:
         self.inputPositions = inputPositions
         self.correctChoices = correctChoices
 
+    @staticmethod
+    def from_dict(obj: any) -> 'Answer':
+        assert isinstance(obj, dict)
+        options = obj.get("options")
+        attributes = obj.get("attributes")
+        inputPositions = obj.get("inputPositions")
+        correctChoices = obj.get("correctChoices")
+        return Answer(options, attributes, inputPositions, correctChoices)
+
 
 class Question:
     """
@@ -35,6 +44,16 @@ class Question:
         self.answer = answer
         self.parentQuestionId = parentQuestionId
 
+    @staticmethod
+    def from_dict(obj: any) -> 'Question':
+        assert isinstance(obj, dict)
+        id = obj.get("id")
+        content = obj.get("content")
+        type = obj.get("type")
+        answer = Answer.from_dict(obj.get("answer"))
+        parentQuestionId = obj.get("parentQuestionId")
+        return Question(id, content, type, answer, parentQuestionId)
+
 
 class Questions:
     """
@@ -43,6 +62,12 @@ class Questions:
 
     def __init__(self, questions: list[Question]):
         self.questions = questions
+
+    @staticmethod
+    def from_dict(obj: any) -> 'Questions':
+        assert isinstance(obj, list)
+        questions = [Question.from_dict(i) for i in obj]
+        return Questions(questions)
 
 
 class Solution:
@@ -54,6 +79,13 @@ class Solution:
         self.input = input
         self.selected = selected
 
+    @staticmethod
+    def from_dict(obj: any) -> 'Solution':
+        assert isinstance(obj, dict)
+        input = obj.get("input")
+        selected = obj.get("selected")
+        return Solution(input, selected)
+
 
 class Solutions:
     """
@@ -62,6 +94,12 @@ class Solutions:
 
     def __init__(self, solutions: list[Solution]):
         self.solutions = solutions
+
+    @staticmethod
+    def from_dict(obj: any) -> 'Solutions':
+        assert isinstance(obj, list)
+        solutions = [Solution.from_dict(i) for i in obj]
+        return Solutions(solutions)
 
 
 class Demographics:
@@ -72,6 +110,13 @@ class Demographics:
     def __init__(self, questions: Questions, solutions: Solutions):
         self.questions = questions
         self.solutions = solutions
+
+    @staticmethod
+    def from_dict(obj: any) -> 'Demographics':
+        assert isinstance(obj, dict)
+        questions = Questions.from_dict(obj.get("questions"))
+        solutions = Solutions.from_dict(obj.get("solutions"))
+        return Demographics(questions, solutions)
 
 
 def load_demographics(input_path: Path = DEFAULT_SURVEY_DIR) -> list[Demographics]:
@@ -85,11 +130,35 @@ def load_demographics(input_path: Path = DEFAULT_SURVEY_DIR) -> list[Demographic
     file_paths = list_files_with_name(input_path, DEMOGRAPHICS_FILE_NAME)
 
     # Load the JSON data
-    json_objects = [Demographics(**load_json_file(file_path)) for file_path in
-                    file_paths]
+    test = Demographics.from_dict(load_json_file(file_paths[0]))
+    json_objects = []
+    for file_path in file_paths:
+        json_objects.append(Demographics.from_dict(load_json_file(file_path)))
 
     return json_objects
 
 
+def combine_demographics(demographics: list[Demographics]) -> Demographics:
+    """
+    Combine multiple demographic objects into a single demographic object
+    :param demographics: The demographic objects
+    :return: A single demographic object
+    """
+    questions = []
+    solutions = []
+
+    for demographic in demographics:
+        # Add the question, if there is no question with the same id
+        for question in demographic.questions.questions:
+            if question.id not in [q.id for q in questions]:
+                questions.append(question)
+
+        # Add all solutions
+        solutions.extend(demographic.solutions.solutions)
+
+    return Demographics(Questions(questions), Solutions(solutions))
+
+
 demographics = load_demographics(SURVEY_DATA_DIR)
+demographics = combine_demographics(demographics)
 print(demographics)
