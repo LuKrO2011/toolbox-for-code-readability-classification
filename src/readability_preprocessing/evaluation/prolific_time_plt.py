914513406db54be632ddae3978a7940601f3788a
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-02-12 21:27:14 +0100

    Fixing time plotting

--- src/readability_preprocessing/evaluation/prolific_time_plt.py
+++ src/readability_preprocessing/evaluation/prolific_time_plt.py
@@ -20,7 +20,7 @@ df = df[~df['Status'].isin(not_completed_codes)]
 df = df[df['Completion code'] != 'NOCODE']
 
 # Get the time taken to complete the survey
-time_taken = pd.to_timedelta(df['Time taken'], unit="s")
+time_taken = pd.to_timedelta(df['Time taken'], unit="s").dropna()
 
 # Calculate and display mean, std, and median
 mean_time = time_taken.mean().total_seconds()
@@ -32,7 +32,28 @@ print(f"Mean time: {mean_time // 60:.0f}:{mean_time % 60:.0f}")
 print(f"Std time: {std_time // 60:.0f}:{std_time % 60:.0f}")
 print(f"Median time: {median_time // 60:.0f}:{median_time % 60:.0f}")
 
-# Plotting (orange line = median)
+# Print as seconds
+print(f"Mean time: {mean_time:.0f} seconds")
+print(f"Std time: {std_time:.0f} seconds")
+print(f"Median time: {median_time:.0f} seconds")
+print(f"Mean - 1 std: {mean_time - std_time:.0f} seconds")
+
+# Plotting a historgram
+# plt.hist(time_taken.dt.total_seconds() // 60, bins=30)
+# plt.xlabel('Time taken (minutes)')
+# plt.ylabel('Number of participants')
+# plt.title('Time taken to complete the survey')
+#
+# # Mean, median, mean - 1 std
+# plt.axvline(mean_time // 60, color='orange', linestyle='dashed', linewidth=2)
+# # plt.axvline((mean_time - std_time) // 60, color='red', linestyle='dashed', linewidth=2)
+#
+# # legend
+# plt.legend(['Mean'])
+#
+# plt.show()
+
+# Plot a box plot (orange line = median)
 time_in_seconds = time_taken.dt.total_seconds()
 plt.figure(figsize=(6, 8))
 plt.boxplot(time_in_seconds)
