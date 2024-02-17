6be67a1c14fa089b93c378e7a87dabf8fbe857b7
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-02-17 14:05:47 +0100

    Refactoring comments remove

--- src/readability_preprocessing/rdh/temp.py
+++ src/readability_preprocessing/rdh/temp.py
@@ -33,58 +33,58 @@ class RemComController:
     def remove_line_comments(self, finaltext):
         lines = finaltext.split('\n')
         for line in lines:
-            rem = line
-            if self.esc == '':
-                self.esc = None
-
-            if self.comment in line:
-                comIndexes = [i for i in range(len(line)) if line.startswith(
-                    self.comment, i)]
-
-                d, s = 0, 0
-                for i in range(len(line)):
-                    if line[i] == '"' and (
-                        not self.esccheck or self.esc == '' or line[i - len(
-                        self.esc):i] != self.esc
-                        or (line[i - len(self.esc):i] == self.esc and line[
-                                                                      i - 2 * len(
-                                                                          self.esc):i - len(
-                                                                          self.esc)] == self.esc)) and d == 0 and s == 0:
-                        d += 1
-                    elif line[i] == '"' and (
-                        not self.esccheck or self.esc == '' or line[i - len(
-                        self.esc):i] != self.esc
-                        or (line[i - len(self.esc):i] == self.esc and line[
-                                                                      i - 2 * len(
-                                                                          self.esc):i - len(
-                                                                          self.esc)] == self.esc)) and d == 1 and s == 0:
-                        d -= 1
-                    if line[i] == "'" and (
-                        not self.esccheck or self.esc == '' or line[i - len(
-                        self.esc):i] != self.esc
-                        or (line[i - len(self.esc):i] == self.esc and line[
-                                                                      i - 2 * len(
-                                                                          self.esc):i - len(
-                                                                          self.esc)] == self.esc)) and d == 0 and s == 0:
-                        s += 1
-                    elif line[i] == "'" and (
-                        not self.esccheck or self.esc == '' or line[i - len(
-                        self.esc):i] != self.esc
-                        or (line[i - len(self.esc):i] == self.esc and line[
-                                                                      i - 2 * len(
-                                                                          self.esc):i - len(
-                                                                          self.esc)] == self.esc)) and d == 0 and s == 1:
-                        s -= 1
-
-                    if i in comIndexes and d == 0 and s == 0:
-                        rem = line[:i]
-                        break
-
+            rem = self.remove_comments_from_line(line)
             if not rem.strip():
                 rem = '\n'
             self.arr.append(rem)
+
         finaltext = '\n'.join(self.arr)
         return finaltext
+
+    def remove_comments_from_line(self, line):
+        if self.esc == '':
+            self.esc = None
+
+        if self.comment in line:
+            comIndexes = [i for i in range(len(line)) if
+                          line.startswith(self.comment, i)]
+            rem = self.extract_comment_free_line(line, comIndexes)
+        else:
+            rem = line
+
+        return rem
+
+    def extract_comment_free_line(self, line, comIndexes):
+        d, s = 0, 0
+        rem = line
+
+        for i in range(len(line)):
+            if self.is_quote_char(line[i], d, s) and self.is_valid_quote(i, line):
+                d, s = self.update_quote_counters(line[i], d, s)
+
+            if i in comIndexes and d == 0 and s == 0:
+                rem = line[:i]
+                break
+
+        return rem
+
+    def is_quote_char(self, char, d, s):
+        return (char == '"' and d == 0 and s == 0) or (
+            char == "'" and d == 0 and s == 0)
+
+    def is_valid_quote(self, i, line):
+        return not self.esccheck or self.esc == '' or line[i - len(
+            self.esc):i] != self.esc or (line[i - len(self.esc):i] == self.esc and line[
+                                                                                   i - 2 * len(
+                                                                                       self.esc):i - len(
+                                                                                       self.esc)] == self.esc)
+
+    def update_quote_counters(self, char, d, s):
+        if char == '"':
+            d += 1
+        elif char == "'":
+            s += 1
+        return d, s
 
     def remove_block_comments(self, finaltext, full):
         bcOpenIndexes, bcCloseIndexes = [], []
