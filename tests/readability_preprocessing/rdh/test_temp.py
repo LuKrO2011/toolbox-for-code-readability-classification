0245d13fd2c5057b32ae92a34e1363c59d8c40ea
LuKrO2011 <47751825+LuKrO2011@users.noreply.github.com>
2024-02-17 15:05:52 +0100

    Fixing comment removing of single lines

--- tests/readability_preprocessing/rdh/test_temp.py
+++ tests/readability_preprocessing/rdh/test_temp.py
@@ -1,10 +1,11 @@
-from src.readability_preprocessing.rdh.temp import RemComController
+from src.readability_preprocessing.rdh.temp import RemoveCommentsController
 from src.readability_preprocessing.utils.utils import load_code
 from tests.readability_preprocessing.utils.utils import COMMENTS_WITH_DIR
 
+
 def test_remove_comments():
-    rem_com_controller = RemComController()
+    rem_com_controller = RemoveCommentsController()
     code = load_code(COMMENTS_WITH_DIR / "helloWorld.java")
-    rem_com_controller.appText = code
-    rem_com_controller.split()
-    print(rem_com_controller.appText)
+    new_code = rem_com_controller.remove_comments(code)
+    print()
+    print(new_code)
